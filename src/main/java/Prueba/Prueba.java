7cf3d91aac058f931380c021b6ab0810b2b02b37
machocus <aguila.elenita@gmail.com>
2021-06-08 22:49:59 +0200

    proyecto modificado de antiguo para que funcione. solo funciona clientes modularizado

--- src/main/java/Prueba/Prueba.java
+++ src/main/java/Prueba/Prueba.java
@@ -22,7 +22,10 @@ public class Prueba {
         menu();
     }
 
-    public static void menu() {
+    public static void menu() { //original
+        //clientes
+        Clientes cliente = new Clientes();
+        ArrayList<Clientes> listaClientes = cliente.leerCliente("clientes.csv");
 
         Scanner teclado = new Scanner(System.in);
         int menu = 0;
@@ -43,20 +46,18 @@ public class Prueba {
             //switch para entrar en el menu que se ha seleccionado anteriormente
             switch (menu) {
                 case 1:
-                    clientes();
+                    clientes(listaClientes);
                     break;
                 case 2:
-                    productos();
+//                    productos();
                     break;
             }
         } while (menu != 7);
 
     }//menu
 
-    public static void clientes() {
+    public static void clientes(ArrayList<Clientes> listaClientes) {
         //Clientes
-        Clientes cliente = new Clientes();
-        ArrayList<Clientes> listaClientes = cliente.leerCliente("clientes.csv");
         Scanner teclado = new Scanner(System.in);
 
         int menuClientes = 0;
@@ -166,119 +167,119 @@ public class Prueba {
         }
     }
 
-    public static void productos() {
-        //Articulos
-        Articulos articulo = new Articulos();
-        ArrayList<Articulos> listaArticulos = articulo.leerArticulo("articulos.csv");
-        //Servicios
-        Servicios servicio = new Servicios();
-        ArrayList<Servicios> listaServicios = servicio.leerServicio("servicios.csv");
-        Scanner teclado = new Scanner(System.in);
-
-        int menuProductos = 0;
-        //do while para filtrar las operaciones
-        do {
-            System.out.println("¿Que operacion desea realizar?\n"
-                    + "1.Consultar\n2.Modificar\n3.Añadir\n4.Borrar");
-            menuProductos = teclado.nextInt();
-        } while (menuProductos < 1 || menuProductos > 4);
-
-        //switch para realizar las operaciones elegidas anteriormente
-        switch (menuProductos) {
-            case 1:
-                System.out.println("Ha elegido consultar");
-                //for para imprimir los clientes
-                for (Productos p : listaProductos) {
-                    System.out.println(c);
-                }
-                break;
-            case 2:
-                System.out.println("Ha elegido modificar");
-                //for para imprimir los clientes
-                for (Clientes c : listaProductos) {
-                    System.out.println(c);
-                }
-                System.out.println("¿Que clientes de los anteriores quiere "
-                        + "modificar?");
-                teclado.nextLine();
-                String nifCliente = teclado.nextLine();
-
-                int comprobacion = 0;
-                //for para modificar el cliente
-                for (Clientes c : listaProductos) {
-                    //if para comprobar el nombre del cliente
-                    if (nifCliente.equals(c.getNifCliente())) {
-                        //se modifica el nombre, apellidos y direccion
-                        System.out.println("Introduce el nombre:");
-                        String nombre = teclado.nextLine();
-                        c.setNombre(nombre);
-
-                        System.out.println("Introduce los apellidos:");
-                        String apellidos = teclado.nextLine();
-                        c.setApellidos(apellidos);
-
-                        System.out.println("Introduce el nif:");
-                        String nif = teclado.nextLine();
-                        c.setNifCliente(nif);
-
-                        System.out.println("Introduce la direccion:");
-                        String direccion = teclado.nextLine();
-                        c.setDireccion(direccion);
-                        comprobacion = 1;
-                    }
-                }
-                if (comprobacion == 0) {
-                    System.out.println("No se ha encontrado el cliente");
-                }
-                break;
-            case 3:
-                System.out.println("Ha elegido añadir");
-                teclado.nextLine();
-                System.out.println("Introduce el nombre:");
-                String nombre = teclado.nextLine();
-                System.out.println("Introduce los apellidos:");
-                String apellidos = teclado.nextLine();
-                System.out.println("Introduce el nif:");
-                String nif = teclado.nextLine();
-                System.out.println("Introduce la direccion:");
-                String direccion = teclado.nextLine();
-
-                int nifExist = 0;
-                //for para comprobar si el dni a meter ya existe
-                for (Clientes cli : listaProductos) {
-                    if (nif.equals(cli.getNifCliente())) {
-                        nifExist = 1;
-                    }
-                }
-                //if para meter el cliente en caso de que el nif no exista
-                if (nifExist == 0) {
-                    listaProductos.add(new Clientes(nombre, apellidos, nif, direccion));
-                    System.out.println("Se ha añadido el cliente");
-                } else {
-                    System.out.println("No se ha añadido el cliente");
-                }
-
-                break;
-            case 4:
-                System.out.println("Ha elegido borrar");
-                System.out.println("Escribe el nif del cliente que "
-                        + "desea borrar");
-                teclado.nextLine();
-                String nifrBorrar = teclado.nextLine();
-                int existecli = 0;
-                for (Clientes cli : listaProductos) {
-                    if (nifrBorrar.equals(cli.getNifCliente())) {
-                        listaProductos.remove(cli);
-                        existecli = 1;
-                    }
-                }
-                if (existecli == 1) {
-                    System.out.println("se ha eliminado el cliente");
-                } else {
-                    System.out.println("El cliente no existe");
-                }
-
-                break;
-        }
-    }
+//    public static void productos() {
+//        //Articulos
+//        Articulos articulo = new Articulos();
+//        ArrayList<Articulos> listaArticulos = articulo.leerArticulo("articulos.csv");
+//        //Servicios
+//        Servicios servicio = new Servicios();
+//        ArrayList<Servicios> listaServicios = servicio.leerServicio("servicios.csv");
+//        Scanner teclado = new Scanner(System.in);
+//
+//        int menuProductos = 0;
+//        //do while para filtrar las operaciones
+//        do {
+//            System.out.println("¿Que operacion desea realizar?\n"
+//                    + "1.Consultar\n2.Modificar\n3.Añadir\n4.Borrar");
+//            menuProductos = teclado.nextInt();
+//        } while (menuProductos < 1 || menuProductos > 4);
+//
+//        //switch para realizar las operaciones elegidas anteriormente
+//        switch (menuProductos) {
+//            case 1:
+//                System.out.println("Ha elegido consultar");
+//                //for para imprimir los clientes
+//                for (Productos p : listaProductos) {
+//                    System.out.println(c);
+//                }
+//                break;
+//            case 2:
+//                System.out.println("Ha elegido modificar");
+//                //for para imprimir los clientes
+//                for (Clientes c : listaProductos) {
+//                    System.out.println(c);
+//                }
+//                System.out.println("¿Que clientes de los anteriores quiere "
+//                        + "modificar?");
+//                teclado.nextLine();
+//                String nifCliente = teclado.nextLine();
+//
+//                int comprobacion = 0;
+//                //for para modificar el cliente
+//                for (Clientes c : listaProductos) {
+//                    //if para comprobar el nombre del cliente
+//                    if (nifCliente.equals(c.getNifCliente())) {
+//                        //se modifica el nombre, apellidos y direccion
+//                        System.out.println("Introduce el nombre:");
+//                        String nombre = teclado.nextLine();
+//                        c.setNombre(nombre);
+//
+//                        System.out.println("Introduce los apellidos:");
+//                        String apellidos = teclado.nextLine();
+//                        c.setApellidos(apellidos);
+//
+//                        System.out.println("Introduce el nif:");
+//                        String nif = teclado.nextLine();
+//                        c.setNifCliente(nif);
+//
+//                        System.out.println("Introduce la direccion:");
+//                        String direccion = teclado.nextLine();
+//                        c.setDireccion(direccion);
+//                        comprobacion = 1;
+//                    }
+//                }
+//                if (comprobacion == 0) {
+//                    System.out.println("No se ha encontrado el cliente");
+//                }
+//                break;
+//            case 3:
+//                System.out.println("Ha elegido añadir");
+//                teclado.nextLine();
+//                System.out.println("Introduce el nombre:");
+//                String nombre = teclado.nextLine();
+//                System.out.println("Introduce los apellidos:");
+//                String apellidos = teclado.nextLine();
+//                System.out.println("Introduce el nif:");
+//                String nif = teclado.nextLine();
+//                System.out.println("Introduce la direccion:");
+//                String direccion = teclado.nextLine();
+//
+//                int nifExist = 0;
+//                //for para comprobar si el dni a meter ya existe
+//                for (Clientes cli : listaProductos) {
+//                    if (nif.equals(cli.getNifCliente())) {
+//                        nifExist = 1;
+//                    }
+//                }
+//                //if para meter el cliente en caso de que el nif no exista
+//                if (nifExist == 0) {
+//                    listaProductos.add(new Clientes(nombre, apellidos, nif, direccion));
+//                    System.out.println("Se ha añadido el cliente");
+//                } else {
+//                    System.out.println("No se ha añadido el cliente");
+//                }
+//
+//                break;
+//            case 4:
+//                System.out.println("Ha elegido borrar");
+//                System.out.println("Escribe el nif del cliente que "
+//                        + "desea borrar");
+//                teclado.nextLine();
+//                String nifrBorrar = teclado.nextLine();
+//                int existecli = 0;
+//                for (Clientes cli : listaProductos) {
+//                    if (nifrBorrar.equals(cli.getNifCliente())) {
+//                        listaProductos.remove(cli);
+//                        existecli = 1;
+//                    }
+//                }
+//                if (existecli == 1) {
+//                    System.out.println("se ha eliminado el cliente");
+//                } else {
+//                    System.out.println("El cliente no existe");
+//                }
+//
+//                break;
+//        }
+//    }
 }
